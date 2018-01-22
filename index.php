bb04935e6852cc432dbd1185e0254d5e64cdfd25
Aditya Soni <adityasoni5859@gmail.com>
2018-01-23 00:45:20 +0530

    Created home index page

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function(){
+            $('.carousel').carousel({
+                interval:50
+            })
+        });
+    </script>
+</head>
+<body class="home_body">
+<nav class="navbar navbar-light bg-default navbar-toggleable-md">
+    <span class="navbar-brand text-danger" href="#">
+        B+Care Ltd.
+    </span>
+    <div class="collapse navbar-collapse" id="containerNavbar">
+        <ul class="navbar-nav ml-auto mr-2">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<hr>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <img class="d-block img-fluid" src="imgs/itest.svg" alt="First slide">
+            <img class="d-block img-fluid" src="imgs/itest.svg" alt="First slide">
+            <img class="d-block img-fluid" src="imgs/itest.svg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block img-fluid" src="imgs/itest.svg" alt="First slide">
+            <img class="d-block img-fluid" src="imgs/itest.svg" alt="First slide">
+            <img class="d-block img-fluid" src="imgs/itest.svg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block img-fluid" src="imgs/itest.svg" alt="First slide">
+            <img class="d-block img-fluid" src="imgs/itest.svg" alt="First slide">
+            <img class="d-block img-fluid" src="imgs/itest.svg" alt="First slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<div class="container">
+    <br>
+    <p>
+        <h1 class="text-center text-danger">Don't waste your blood, Donate !</h1>
+        <p class="text-center">
+            Many accident victims, innocent children suffering from thalassaemia, pregnant women, cancer patients and
+            other people in life threatening situations die due to shortage of blood.
+        Here in "B+Care" we value life as a foremost give given to mankind, and therefore we preserve and supply
+            blood whenever and whereever needed. So that no life could go wasted without an effort.
+        </p>
+    </p>
+</div>
+<div class="container text-center mt-5">
+        <button type="button" class="btn btn-outline-danger btn-lg mr-5">&nbsp;&nbsp;&nbsp;Login&nbsp;&nbsp;&nbsp;</button>
+        <button type="button" class="btn btn-outline-warning btn-lg ml-5">&nbsp;&nbsp;&nbsp;Register&nbsp;&nbsp;&nbsp;</button>
+</div>
+<div class="bg-danger mt-5">
+    <div class="container text-center text-white">
+        All Rights Rserved by Copyright-XXXX @ 2018
+    </div>
+</div>
+</body>
+</html>
